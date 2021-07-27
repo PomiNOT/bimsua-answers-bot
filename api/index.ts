8d379b81424840ff97da01b65a4f3f2b1dede5a9
PomiNOT <ppminet@protonmail.com>
2021-07-27 17:07:26 +0700

    test 2

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,9 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import app from './app';
+import app from '../src/app';
 
-import ping from './commands/opensheet';
+import ping from '../src/commands/opensheet';
 app.register(ping);
 
 app.server.listen(5000, () => console.log('Listening on 5000'));
